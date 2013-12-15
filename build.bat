c0d6d8d10a9015649ee7cf47ad617697283a3ec6
vishak venkateswaran <vishakvkt@gmail.com>
2013-12-15 15:31:36 +0000

    first commit

---
+++ build.bat
@@ -0,0 +1,17 @@
+@Echo off
+ECHO. cleaning release..
+if not exist mkdir release
+
+CALl del release\ /s /q
+ECHO. Zipping app to release\app.nw...
+CALL cd /D app\
+CALL ..\Tools\7Zip\7za.exe a -tzip ..\release\app.nw *.* -r
+cALL cd ..\
+ECHO. Linking app to node-webkit and building executable...
+CALL copy /b Tools\node-webkit\nw.exe + release\app.nw release\app.exe
+CALL copy Tools\node-webkit\nw.pak release\
+CALL copy Tools\node-webkit\*.dll release\
+ECHO. All good. Build successful. App in release folder.
+PAUSE
+
+
