d40921c7b413266320a1fe1fac5540c1ff9cb7df
vishak venkateswaran <vishakvkt@gmail.com>
2013-12-15 21:29:22 +0000

    added windowless frame to sample package.json.

--- build.bat
+++ build.bat
@@ -6,12 +6,12 @@ CALl del release\ /s /q
 ECHO. Zipping app to release\app.nw...
 CALL cd /D app\
 CALL ..\Tools\7Zip\7za.exe a -tzip ..\release\app.nw *.* -r
-cALL cd ..\
+CALL cd ..\
 ECHO. Linking app to node-webkit and building executable...
 CALL copy /b Tools\node-webkit\nw.exe + release\app.nw release\app.exe
 CALL copy Tools\node-webkit\nw.pak release\
 CALL copy Tools\node-webkit\*.dll release\
-ECHO. All good. Build successful. App in release folder.
+ECHO. All good. Build successful. app.exe in release folder.
 PAUSE
 
 
