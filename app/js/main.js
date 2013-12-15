216d3d8ff9ac2fb84148ed0fccbdd9ee9e50da22
vishak venkateswaran <vishakvkt@gmail.com>
2013-12-15 23:26:46 +0000

    added node and webkit logos. added jquery and created proper example. Also added f12 dev console

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
-(function($) {
-
-    var $s = $('.progress');
-    $s.fadeIn('slow');
+(function($) {    
+    var f12 = 123;
+    $('img').fadeIn('slow');
+    $('body').on('keyup', function(e) {
+        if(e.which === f12) {
+            require('nw.gui').Window.get().showDevTools();
+        }
+    });    
 })(jQuery);
